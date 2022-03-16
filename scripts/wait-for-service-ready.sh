ecf6ec8631bb4943192efd306fd02b8170399b43
noelsev <noel.sev@gmail.com>
2022-03-17 00:56:07 +1300

    Move cucumber tests to cucumber

---
+++ scripts/wait-for-service-ready.sh
@@ -0,0 +1,26 @@
+# Usage:
+# ./wait_for_service_ready.sh http://localhost:5025/health/endpoint 200 service-name
+
+URL=$1
+SERVICE_NAME=$2
+SUCCESS_CODES='200,204'
+TIMEOUT=180
+
+echo "Waiting for service ${SERVICE_NAME} to be ready at: ${URL}"
+
+COUNT=0
+until [ -z "${SUCCESS_CODES##*$(curl -s -o /dev/null -w "%{http_code}" $URL)*}" ] || [ $COUNT -gt $TIMEOUT ]; do
+  printf '.'
+  sleep 1
+  COUNT=$((COUNT+1))
+done
+
+if [ $COUNT -gt $TIMEOUT ]; then
+  echo ""
+  echo "ERROR: Timeout waiting for service ${SERVICE_NAME} at: ${URL}"
+  exit 1
+else
+  echo ""
+  echo "Service ${SERVICE_NAME} is ready at: ${URL}"
+  exit 0
+fi
